b5e6b9f05025ef898d0939dc75935bb0af757210
IgorErin <erin.igore@yandex.ru>
2024-02-18 13:37:18 +0300

    add: auto qtree mtx prepend

--- app/Main.hs
+++ app/Main.hs
@@ -1,39 +1,75 @@
 module Main (main) where
 
+import Prelude hiding (writeFile, readFile)
+
 import Options.Applicative
-import Reader (run)
-import QTree (toBuilder, explicitZeros)
-import Data.Text.Lazy.IO (putStr)
 import Fmt ((+|), (|+))
+
+import Data.Text.Lazy.IO (writeFile, readFile)
 import Data.Text.Lazy (Text)
 
-import Prelude hiding (putStr)
+-- only for file search
+import Test.Tasty.Golden (findByExtension)
+import System.FilePath (replaceDirectory, replaceExtension, (</>), )
 
-inputParser :: Parser FilePath
-inputParser = strOption
-    (long "path"
-    <> short 'p'
-    <> help "Path to mtx file")
+import qualified Reader (run)
+import QTree (QTree, toText, explicitZeros)
 
-inputInfo :: ParserInfo FilePath
+data Input = MkInput { dstPath :: FilePath }
+
+inputParser :: Parser Input
+inputParser = MkInput
+    <$> strOption
+        (long "dstpath"
+        <> short 'd'
+        <> help "Destination path")
+
+inputInfo :: ParserInfo Input
 inputInfo = info inputParser
     ( fullDesc
     <> progDesc "Mtx to quad tree matrix converter" )
 
-varName :: Text
-varName = "matrix"
+matrixName :: Text
+matrixName = "matrix"
 
+srcPaths :: IO [FilePath]
+srcPaths = findByExtension [".mtx"] "dataset"
 
+hvlLibPath :: FilePath
+hvlLibPath = "hvl" </> "lib"
+
+translateMtx :: QTree (Maybe Int) -> Text
+translateMtx mtx =
+    let expMtx = explicitZeros 0 mtx
+    in toText expMtx
+
+prependProgram :: Text -> Text -> Text -> Text
+prependProgram varName matrix code = ""+|varName|+" = "+|matrix|+"\n"+|code|+""
+
+mkDstPath :: FilePath -> FilePath -> FilePath
+mkDstPath fp dst = (flip replaceExtension) "t" $ replaceDirectory fp dst
+
+processMtx :: FilePath -> Text -> FilePath -> IO ()
+processMtx dstPath code src = do
+    mtx <- Reader.run src
+
+    let mtxText = translateMtx mtx
+    let code' = prependProgram matrixName mtxText code
+    let dstPath' = mkDstPath src dstPath
+
+    print dstPath'
+
+    writeFile dstPath' code'
+
+run :: FilePath -> IO ()
+run dst = do
+    hvlCode <- readFile hvlLibPath
+    srcPaths' <- srcPaths
+
+    mapM_ (processMtx dst hvlCode) srcPaths'
 
 main :: IO ()
 main = do
-    mtxFilePath <- execParser inputInfo
+    MkInput { dstPath } <- execParser inputInfo
 
-    -- hardcoded implicite zeros elimination. rerork
-    qtMtx <- explicitZeros 0 <$> run mtxFilePath
-    let qtText = toBuilder qtMtx
-
-    -- hardcoded binding. rework
-    let result = ""+|varName|+" = "+|qtText|+""
-
-    putStr result
+    run dstPath
