90ae00c4c0a8f753c83e951faa2ca45577523556
IgorErin <erin.igore@yandex.ru>
2024-04-25 22:25:50 +0300

    refactor: reader

--- src/Reader.hs
+++ src/Reader.hs
@@ -2,10 +2,12 @@ module Reader(run) where
 
 import Data.Matrix.MatrixMarket (readMatrix)
 import qualified Convert (run)
-import QTree (QTree)
+import QTree (QTree, mergeQTree)
 
 run :: FilePath -> IO (QTree (Maybe Int))
 run fp = do
+    putStrLn $ "reading: " ++ fp
     mtx <- readMatrix fp
+    let converted = mergeQTree $ Convert.run mtx
 
-    return $ (const 1 <$>) <$> Convert.run mtx
+    return $ (const 1 <$>) <$> converted
